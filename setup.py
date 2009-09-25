4add4141122bf03fa66c5f826c8245fcc74802eb
Jeremy Johnstone <jstone@stormtrip-dm.corp.yahoo.com>
2009-09-25 12:38:57 -0700

    - Snow Leopard 10.6.1 compatibility
    Import from upstream source:
    http://code.google.com/p/quotefixformac/downloads/detail?name=QuoteFix-27.tar.gz

--- setup.py
+++ setup.py
@@ -20,8 +20,12 @@ setup(
             CFBundleIdentifier                  = 'name.klep.mail.QuoteFix',
             NSHumanReadableCopyright            = '(c) 2009, Robert Klep, robert@klep.name',
             SupportedPluginCompatibilityUUIDs   = [
+                # 10.6
                 '225E0A48-2CDB-44A6-8D99-A9BB8AF6BA04', # Mail 4.0
                 'B3F3FC72-315D-4323-BE85-7AB76090224D', # Message.framework 4.0
+                # 10.6.1
+                '99BB3782-6C16-4C6F-B910-25ED1C1CB38B',
+                '2610F061-32C6-4C6B-B90A-7A3102F9B9C8',
             ]
         )
     ))
